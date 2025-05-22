82775bdf6967a6a6c8b646009178fcb7fefd1cca
linasalamanca <118871999+linasalamanca@users.noreply.github.com>
2025-05-21 23:14:22 -0500

    visita

--- src/main/java/co/edu/javeriana/sv_visits/Controller/ActividadPacienteVisitaController.java
+++ src/main/java/co/edu/javeriana/sv_visits/Controller/ActividadPacienteVisitaController.java
@@ -1,7 +1,7 @@
 package co.edu.javeriana.sv_visits.Controller;
 
-import co.edu.javeriana.sv_visits.Entity.ActividadPacienteVisitaEntity;
-import co.edu.javeriana.sv_visits.Service.ActividadPacienteVisitaService;
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -9,7 +9,8 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.Optional;
+import co.edu.javeriana.sv_visits.Entity.ActividadPacienteVisitaEntity;
+import co.edu.javeriana.sv_visits.Service.ActividadPacienteVisitaService;
 
 @RestController
 @RequestMapping("/actividad-paciente-visita")
